c4a798a1d42f5d0f1fcef4e7b41ad5d79c26e6c7
liobarrozo <liobarrozo8@gmail.com>
2025-03-19 00:35:55 -0300

    fixFecha

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+//globos
+
 // Configuramos la fecha objetivo inicial
-const targetDate = new Date("2025-01-14T00:00:00").getTime();
+const targetDate = new Date("2025-04-14T00:00:00").getTime();
 
 let lastValues = {
-    days: null,
-    hours: null,
-    minutes: null,
-    seconds: null
+  days: null,
+  hours: null,
+  minutes: null,
+  seconds: null,
 };
 
 let meses = 10; // Variable global para meses
@@ -13,86 +15,103 @@ let ejecucionMensual = 0; // Control para la ejecución mensual
 let stopCounter = false; // Variable para detener el contador
 
 function updateCounter() {
-    if (stopCounter) return; // Detenemos la ejecución si corresponde
+  if (stopCounter) return; // Detenemos la ejecución si corresponde
 
-    const now = new Date().getTime();
-    const distance = targetDate - now;
+  const now = new Date().getTime();
+  const distance = targetDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Si es el día 14 y el tiempo restante es 0 o menor, detenemos el contador
-    if (distance <= 0 && new Date().getDate() === 14) {
-        stopCounter = true;
-        setToZero();
-        changeH2(meses); // Actualizamos el texto del H2
-        setTimeout(() => {
-            stopCounter = false;
-            targetDate += 30 * 24 * 60 * 60 * 1000; // Actualiza la fecha objetivo al próximo mes
-        }, calculateTimeUntilMidnight());
-        return;
-    }
+  // Si es el día 14 y el tiempo restante es 0 o menor, detenemos el contador
+  if (distance <= 0 && new Date().getDate() === 14) {
+    stopCounter = true;
+    setToZero();
+    changeH2(meses); // Actualizamos el texto del H2
 
-    // Actualizamos los dígitos solo si hay un cambio
-    updateDigit("days", days);
-    updateDigit("hours", hours);
-    updateDigit("minutes", minutes);
-    updateDigit("seconds", seconds);
+    setTimeout(() => {
+      stopCounter = false;
+      targetDate += 30 * 24 * 60 * 60 * 1000; // Actualiza la fecha objetivo al próximo mes
+    }, calculateTimeUntilMidnight());
+    return;
+  }
 
-    meses = updateMeses(); // Actualizamos el valor de meses
+  // Actualizamos los dígitos solo si hay un cambio
+  updateDigit("days", days);
+  updateDigit("hours", hours);
+  updateDigit("minutes", minutes);
+  updateDigit("seconds", seconds);
+  changeTextPostCounter(meses); // Actualizamos el texto del H3
+  meses = updateMeses(); // Actualizamos el valor de meses
 }
 
 function setToZero() {
-    updateDigit("days", 0);
-    updateDigit("hours", 0);
-    updateDigit("minutes", 0);
-    updateDigit("seconds", 0);
+  updateDigit("days", 0);
+  updateDigit("hours", 0);
+  updateDigit("minutes", 0);
+  updateDigit("seconds", 0);
 }
 
 function calculateTimeUntilMidnight() {
-    const now = new Date();
-    const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
-    return midnight - now;
+  const now = new Date();
+  const midnight = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + 1,
+    0,
+    0,
+    0
+  );
+  return midnight - now;
 }
 
 function updateDigit(id, value) {
-    const digitElement = document.getElementById(id);
-    const newValue = value < 10 ? "0" + value : value;
+  const digitElement = document.getElementById(id);
+  const newValue = value < 10 ? "0" + value : value;
 
-    if (lastValues[id] !== newValue) {
-        digitElement.classList.add("flip");
-        digitElement.innerText = newValue;
+  if (lastValues[id] !== newValue) {
+    digitElement.classList.add("flip");
+    digitElement.innerText = newValue;
 
-        setTimeout(() => {
-            digitElement.classList.remove("flip");
-        }, 600);
+    setTimeout(() => {
+      digitElement.classList.remove("flip");
+    }, 600);
 
-        lastValues[id] = newValue;
-    }
+    lastValues[id] = newValue;
+  }
 }
 
 function changeH2(meses) {
-    const diaDelMes = new Date().getDate();
-    const h2 = document.querySelector('h2');
+  const diaDelMes = new Date().getDate();
+  const h2 = document.querySelector("h2");
 
-    if (diaDelMes === 14) {
-        h2.textContent = `¡Felices ${meses} meses!`;
-    }
+  if (diaDelMes === 14) {
+    h2.textContent = `¡Felices ${meses} meses!`;
+  }
+}
+
+function changeTextPostCounter(meses) {
+  const diaDelMes = new Date().getDate();
+  const h3 = document.getElementById("text__post-counter");
+
+  h3.textContent = `Gracias por estos ${meses} meses de mucho amor`;
 }
 
 function updateMeses() {
-    const diaDelMes = new Date().getDate();
+  const diaDelMes = new Date().getDate();
 
-    if (diaDelMes === 14 && ejecucionMensual === 0) {
-        meses++;
-        ejecucionMensual = 1;
-    } else if (diaDelMes === 1) {
-        ejecucionMensual = 0;
-    }
+  if (diaDelMes === 14 && ejecucionMensual === 0) {
+    meses++;
+    ejecucionMensual = 1;
+  } else if (diaDelMes === 1) {
+    ejecucionMensual = 0;
+  }
 
-    return meses;
+  return meses;
 }
 
 // Iniciar el contador cada segundo
