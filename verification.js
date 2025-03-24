fc09d631b5ac8105a5a9d594a530a745549efbeb
liobarrozo <liobarrozo8@gmail.com>
2025-03-23 22:26:44 -0300

    AddDownloadImage

--- verification.js
+++ verification.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("flores-amarillas.mp3").pause();
+  document.getElementById("flores-amarillas.mp3").load();
+});
 function verificarRespuestas() {
   const respuesta1 = document.getElementById("pregunta1").value.toLowerCase();
   const respuesta2 = document.getElementById("pregunta2").value.toLowerCase();
@@ -6,7 +10,7 @@ function verificarRespuestas() {
   const respuestaCorrecta2 = "morado";
 
   if (respuesta1 === respuestaCorrecta1 && respuesta2 === respuestaCorrecta2) {
-    document.getElementById("contenido-revelado").style.display = "block";
+    document.getElementById("contenido-revelado").style.display = "flex";
     alert("Respuestas correctas!");
     document.getElementById("presentation").style.display = "none";
     document.getElementById("formulario-gift").style.display = "none";
